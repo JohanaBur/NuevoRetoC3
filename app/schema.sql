e0057333de25e20ee9578d4a3c615cea2834ba26
Carlos Velasco <carlosandresvelasco@outlook.com>
2022-10-05 18:26:49 -0500

    ocultando credenciales

--- app/schema.sql
+++ app/schema.sql
@@ -52,4 +52,4 @@ CREATE TABLE credentials(
   password TEXT NOT NULL
 );
 
-INSERT INTO credentials (name,user,password) VALUES ('EMAIL_APP','cvelascoa@uninorte.edu.co', 'uQTmjKtgv56svJu');
+INSERT INTO credentials (name,user,password) VALUES ('EMAIL_APP','email@uninorte.edu.co', 'password');
